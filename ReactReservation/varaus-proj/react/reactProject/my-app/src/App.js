3ef4b7817c51cfd01f0e86572412599cfc7452ca
bestoepe <erno.ratto@edu.salpaus.fi>
2023-08-02 07:32:11 +0000

    firsdt eirted

--- ReactReservation/varaus-proj/react/reactProject/my-app/src/App.js
+++ ReactReservation/varaus-proj/react/reactProject/my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 //import Background from './content/background.js'
 import './style.css'
 import Reservation from './content/Reserve.js'
-
+//vmamkakm
 
 
 export default function App() {
